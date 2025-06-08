4a1ccd4760c1dbd5507b8850350d124604301023
hamada <bhak6150@gmail.com>
2025-06-08 21:14:29 +0200

    create booking

--- Taxi/Controllers/BookingsController.cs
+++ Taxi/Controllers/BookingsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Taxi.Data;
+using Taxi.Models;
 
 namespace Taxi.Controllers
 {
@@ -11,5 +12,27 @@ namespace Taxi.Controllers
             var bookings = context.Bookings.ToList();
             return View(bookings);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public RedirectToActionResult Add(Booking request)
+        {
+                context.Bookings.Add(request);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+        }
+
+
+        //public IActionResult Details(int id)
+        //{
+        //    var booking = context.Bookings.FirstOrDefault(b => b.Id == id);
+        //    if (booking == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(booking);
+        //}
     }
 }
